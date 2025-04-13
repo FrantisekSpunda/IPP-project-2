d889d19fae0f242a29e7ded02bd07cbc4ce0aff1
František Špunda <spundafr@gmail.com>
2025-04-13 20:12:20 +0200

    trying hard

---
+++ my_readme.md
@@ -0,0 +1,5 @@
+načtení
+výstup
+error handeling
+
+https://github.com/Kubikuli/IPP_proj2-tests/blob/main/test_valid.py
--- student/Interpreter.php
+++ student/Interpreter.php
@@ -5,15 +5,14 @@ namespace IPP\Student;
 use IPP\Core\AbstractInterpreter;
 use IPP\Core\Exception\NotImplementedException;
 
-class Interpreter extends AbstractInterpreter
-{
-    public function execute(): int
-    {
+class Interpreter extends AbstractInterpreter {
+    public function execute(): int {
         // TODO: Start your code here
         // Check IPP\Core\AbstractInterpreter for predefined I/O objects:
-        // $dom = $this->source->getDOMDocument();
-        // $val = $this->input->readString();
-        // $this->stdout->writeString("stdout");
+        $dom = $this->source->getDOMDocument()->documentElement->tagName;
+        echo ($dom);
+
+
         // $this->stderr->writeString("stderr");
         throw new NotImplementedException();
     }
